926636736c80413c5a73609ad32b1b8b7dbf31b6
Marco Tulio <marcotulio270903@gmail.com>
2025-09-20 14:24:54 -0300

    Saida do programa

---
+++ chatbot/scrapper.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def extrair_tags_article(url: str):
+    print(f"Iniciando busca por tags <article> em: {url}\n")
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+
+    try:
+        response = requests.get(url, headers=headers, timeout=10)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.content, "html.parser")
+        lista_de_articles = soup.find_all("article")
+
+        if not lista_de_articles:
+            raise "Nenhuma tag <article> foi encontrada"
+
+        for i, article_tag in enumerate(lista_de_articles, 1):
+            texto_do_article = article_tag.get_text(separator="\n", strip=True)
+            return texto_do_article
+
+    except requests.exceptions.RequestException as e:
+        raise f"Erro de conexão ao tentar acessar a URL: {e}"
+    except Exception as e:
+        raise f"Ocorreu um erro inesperado: {e}"
